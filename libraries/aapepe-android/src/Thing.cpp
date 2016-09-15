1878f36efe0738ace9341d027b48b157501322f7
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2016-09-15 13:44:39 -0300

    Added library in gvr build

---
+++ libraries/aapepe-android/src/Thing.cpp
@@ -0,0 +1,11 @@
+#include "Thing.h"
+ 
+// Date constructor
+Thing::Thing()
+{
+}
+ 
+// Thing member function
+void Thing::Hi()
+{
+}
---
+++ libraries/aapepe-android/src/Thing.h
@@ -0,0 +1,14 @@
+#ifndef THING_H
+#define THING_H
+ 
+class Thing
+{
+private:
+ 
+public:
+    Thing();
+ 
+    void Hi();
+};
+ 
+#endif
